baa5ba7b972b359a812b3515159aef2c9d8eecf6
maabed <veto44@gmail.com>
2019-08-16 02:51:59 +0300

    DEV-2593: setup routes, auth plugs and basic auth

--- lib/talk_web/router.ex
+++ lib/talk_web/router.ex
@@ -1,11 +1,46 @@
 defmodule TalkWeb.Router do
   use TalkWeb, :router
+  use Plug.ErrorHandler
+  require Logger
 
-  pipeline :api do
+  @env Mix.env()
+
+  pipeline :graphql do
     plug :accepts, ["json"]
+    plug TalkWeb.Plug.AuthPipeline
   end
 
-  scope "/api", TalkWeb do
-    pipe_through :api
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :basic_auth do
+    plug(BasicAuth, use_config: {:talk, :basic_auth})
+  end
+
+
+  scope "/graphql" do
+    pipe_through :graphql
+
+    forward "/", Absinthe.Plug, schema: TalkWeb.Schema
+  end
+
+  scope "/graphiql" do
+    if @env == :prod, do: pipe_through(:basic_auth)
+
+    if @env == :dev do
+      forward "/", Absinthe.Plug.GraphiQL,
+        schema: TalkWeb.Schema,
+        socket: TalkWeb.UserSocket,
+        interface: :playground,
+        default_url: "/graphql"
+    end
+  end
+
+  scope "/health-check" do
+    forward "/", TalkWeb.HealthChecks
   end
 end
