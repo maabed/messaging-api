9c8bf40a4d64c745fa77873f7d8faa174ea2c73a
maabed <veto44@gmail.com>
2019-08-16 03:42:22 +0300

    DEV-2591: subs types and events module

---
+++ lib/talk/events/events.ex
@@ -0,0 +1,101 @@
+defmodule Talk.Events do
+  @moduledoc """
+  This module encapsulates behavior for publishing messages to listeners
+  """
+  require Logger
+  alias Talk.Schemas.{Group, Message, MessageReaction, User}
+
+  # group events
+  def group_created(ids, %Group{} = group) do
+    publish_to_many_users(ids, :group_created, %{group: group})
+  end
+
+  def group_updated(ids, %Group{} = group) do
+    publish_to_many_users(ids, :group_updated, %{group: group})
+  end
+
+  def group_closed(ids, %Group{} = group) do
+    publish_to_many_users(ids, :group_closed, %{group: group})
+  end
+
+  def group_bookmarked(id, %Group{} = group) do
+    publish_to_user(id, :group_bookmarked, %{group: group})
+  end
+
+  def group_unbookmarked(id, %Group{} = group) do
+    publish_to_user(id, :group_unbookmarked, %{group: group})
+  end
+
+  def subscribed_to_group(id, %Group{} = group, %User{} = user) do
+    publish_to_group(id, :subscribed_to_group, %{group: group, user: user})
+  end
+
+  def unsubscribed_from_group(id, %Group{} = group, %User{} = user) do
+    publish_to_group(id, :unsubscribed_from_group, %{group: group, user: user})
+  end
+
+  def group_muted(id, %Group{} = group, %User{} = user) do
+    publish_to_group(id, :group_muted, %{group: group, user: user})
+  end
+
+  def group_archived(id, %Group{} = group, %User{} = user) do
+    publish_to_group(id, :group_archived, %{group: group, user: user})
+  end
+
+  # message events
+  def message_created(ids, %Message{} = message) do
+    publish_to_many_users(ids, :message_created, %{message: message})
+  end
+
+  def message_updated(ids, %Message{} = message) do
+    publish_to_many_users(ids, :message_updated, %{message: message})
+  end
+
+  def message_deleted(ids, %Message{} = message) do
+    publish_to_many_users(ids, :message_deleted, %{message: message})
+  end
+
+  def messages_subscribed(id, messages) do
+    publish_to_user(id, :messages_subscribed, %{messages: messages})
+  end
+
+  def messages_unsubscribed(id, messages) do
+    publish_to_user(id, :messages_unsubscribed, %{messages: messages})
+  end
+
+  def messages_marked_as_read(id, messages) do
+    publish_to_user(id, :messages_marked_as_read, %{messages: messages})
+  end
+
+  def messages_marked_as_unread(id, messages) do
+    publish_to_user(id, :messages_marked_as_unread, %{messages: messages})
+  end
+
+  def message_reaction_created(ids, %Message{} = message, %MessageReaction{} = reaction) do
+    publish_to_many_users(ids, :message_reaction_created, %{message: message, reaction: reaction})
+  end
+
+  def message_reaction_deleted(ids, %Message{} = message, %MessageReaction{} = reaction) do
+    publish_to_many_users(ids, :message_reaction_deleted, %{message: message, reaction: reaction})
+  end
+
+  defp publish_to_user(id, type, payload) do
+    publish(Map.merge(payload, %{type: type}), user_subscription: id)
+  end
+
+  defp publish_to_many_users(ids, type, payload) do
+    topics = Enum.map(ids, fn id -> {:user_subscription, id} end)
+    publish(Map.merge(payload, %{type: type}), topics)
+  end
+
+  defp publish_to_group(id, type, payload) do
+    publish(Map.merge(payload, %{type: type}), group_subscription: id)
+  end
+
+  defp publish(payload, topics) do
+    Logger.debug "publishing user event topics: #{inspect topics}"
+    Logger.debug "publishing user event payload: #{inspect payload}"
+    # @TODO: needs subscriptions and socket to be done first
+    # Absinthe.Subscription.publish(TalkWeb.Endpoint, payload, topics)
+  end
+end
---
+++ lib/talk_web/graphql/types/subscriptions.ex
@@ -0,0 +1,161 @@
+defmodule TalkWeb.Type.Subscriptions do
+   @moduledoc "Subscriptions types for all app models"
+
+  use Absinthe.Schema.Notation
+
+  alias Talk.Groups
+
+  @desc "The users and messages topic response."
+  union :user_subscription_response do
+    types [
+      :group_created_response,
+      :group_updated_response,
+      :message_created_response,
+      :message_updated_response,
+      :message_closed_response,
+      :message_deleted_response,
+      :group_bookmarked_response,
+      :group_unbookmarked_response,
+      :messages_subscribed_response,
+      :messages_unsubscribed_response,
+      :messages_marked_as_read_response,
+      :messages_marked_as_unread_response,
+      :message_reaction_created_response,
+      :message_reaction_deleted_response
+    ]
+    resolve_type &type_resolver/2
+  end
+
+  @desc "The groups topic response."
+  union :group_subscription_response do
+    types [
+      :subscribed_to_group_response,
+      :unsubscribed_from_group_response
+    ]
+    resolve_type &type_resolver/2
+  end
+
+  # Objects
+  object :group_updated_response do
+    field :group, non_null(:group)
+  end
+
+  object :group_bookmarked_response do
+    field :group, non_null(:group)
+  end
+
+  object :group_unbookmarked_response do
+    field :group, non_null(:group)
+  end
+
+  object :group_created_response do
+    field :group, :group
+  end
+
+  object :message_created_response do
+    field :message, :message
+  end
+
+  object :message_updated_response do
+    field :message, :message
+  end
+
+  object :message_closed_response do
+    field :message, :message
+  end
+
+  object :message_deleted_response do
+    field :message, :message
+  end
+
+  object :messages_subscribed_response do
+    field :messages, list_of(:message)
+  end
+
+  object :messages_unsubscribed_response do
+    field :messages, list_of(:message)
+  end
+
+  object :messages_marked_as_unread_response do
+    field :messages, list_of(:message)
+  end
+
+  object :messages_marked_as_read_response do
+    field :messages, list_of(:message)
+  end
+
+  object :subscribed_to_group_response do
+    field :group, non_null(:group)
+    field :user, non_null(:user)
+  end
+
+  object :unsubscribed_from_group_response do
+    field :group, non_null(:group)
+    field :user, non_null(:user)
+  end
+
+  object :message_reaction_created_response do
+    field :message, :message
+    field :reaction, :message_reaction
+  end
+
+  object :message_reaction_deleted_response do
+    field :message, :message
+    field :reaction, :message_reaction
+  end
+
+  # subscriptions
+  object :subscriptions do
+    subscription do
+      @desc "Triggered when a users/messages related event occurs."
+      field :user_subscription, :user_subscription_response do
+        config fn _, %{context: %{user: user}} ->
+          {:ok, topic: user.id}
+        end
+      end
+
+      @desc "Triggered when a groups related event occurs."
+      field :group_subscription, :group_subscription_response do
+        arg :group_id, non_null(:id)
+
+        config fn %{group_id: id}, %{context: %{user: user}} ->
+          case Groups.get_group(user, id) do
+            {:ok, group} ->
+              {:ok, topic: group.id}
+
+            err ->
+              err
+          end
+        end
+      end
+      # field :user_subscription, :user_subscription_response do
+      #   arg :user_id, non_null(:id)
+
+      #   config fn %{user_id: id}, %{context: %{user: user}} ->
+      #     case Users.get_user(user, id) do
+      #       {:ok, user} ->
+      #         if user.user_id == user.id do
+      #           {:ok, topic: id}
+      #         else
+      #           {:error, "Not authorized"}
+      #         end
+
+      #       err ->
+      #         err
+      #     end
+      #   end
+      # end
+    end
+  end
+
+  defp type_resolver(%{type: type}, _) do
+    type
+    |> Atom.to_string()
+    |> concat("_response")
+    |> String.to_atom()
+  end
+
+  defp concat(a, b) do
+    a <> b
+  end
+end
