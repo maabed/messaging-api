819447f1c84529a781b911cf58f86336d3bdafc3
maabed <veto44@gmail.com>
2019-08-16 03:46:30 +0300

    DEV-2591: users ws and jwt auth

--- lib/talk_web/channels/user_socket.ex
+++ lib/talk_web/channels/user_socket.ex
@@ -1,33 +1,37 @@
 defmodule TalkWeb.UserSocket do
+  @moduledoc false
+
   use Phoenix.Socket
+  use Absinthe.Phoenix.Socket, schema: TalkWeb.Schema
+
+  alias TalkWeb.Auth
+  alias TalkWeb.Plug.Graphql
+  alias Absinthe.Phoenix.Socket
 
   ## Channels
-  # channel "room:*", TalkWeb.RoomChannel
+  channel "messages:*", TalkWeb.MessageChannel
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"Authorization" => auth}, socket) do
+    with "Bearer " <> token <- auth, {:ok, %{user: user}} <- Auth.resource_from_token(token) do
+      socket_with_opts =
+        socket
+        |> put_options(user)
+        |> assign(:user, user)
+
+      {:ok, socket_with_opts}
+    else
+      _ -> :error
+    end
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     TalkWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
-  def id(_socket), do: nil
+  def connect(_params, _socket), do: :error
+
+  defp put_options(socket, user) do
+    Socket.put_options(socket,
+      context: Graphql.build_context(user)
+    )
+  end
+
+  # def id(_socket), do: nil
+  def id(socket), do: "user_socket:#{socket.assigns.user.id}"
 end
